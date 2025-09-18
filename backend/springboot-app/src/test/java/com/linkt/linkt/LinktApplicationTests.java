9ecdb45178af27e5f9d7826b2965c801492c72c9
Peter Fitopoulos <peten2005@gmail.com>
2025-09-17 23:39:24 -0400

    Created the springboot-app folder for backend

---
+++ backend/springboot-app/src/test/java/com/linkt/linkt/LinktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkt.linkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
