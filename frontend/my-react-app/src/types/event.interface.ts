873c83a2f799d2a1d874dd25e8e578feb7be916c
Levon Kadehjian <levon.kadehjian.dev@gmail.com>
2025-10-02 22:16:02 -0400

    add Event interface and delete event class

---
+++ frontend/my-react-app/src/types/event.interface.ts
@@ -0,0 +1,14 @@
+interface Event {
+    eventID: number;
+    title: string;
+    description: string;
+    category: string;
+    image : string[];
+    price: number;
+    startDate: Date;
+    endDate: Date;
+    location: string;
+    capacity: number;
+
+
+}
