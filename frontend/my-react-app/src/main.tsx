9dc68afa53fe0baed1a094e65302b27bf6f993d7
Levon Kadehjian <levon.kadehjian.dev@gmail.com>
2025-10-07 19:01:01 -0400

    Add React Router with CheckoutPage route setup

--- frontend/my-react-app/src/main.tsx
+++ frontend/my-react-app/src/main.tsx
@@ -2,13 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/Header.tsx";
 import "./index.css";
-import App from "./App.tsx";
+
+import HomePage from "./HomePage.tsx";
 import { Toolbar } from "@mui/material";
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import CheckoutPage from "./components/CheckoutPage.tsx";
+
+const router = createBrowserRouter([
+    {path: '/',element:<HomePage/>},
+    {path: '/checkoutpage',element:<CheckoutPage/>}
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
-    <Toolbar />
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>
 );
