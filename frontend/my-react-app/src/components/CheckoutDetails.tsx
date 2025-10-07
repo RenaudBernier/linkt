9a7f1d4aebc65880f6d072ef372f8d17dbfa7b39
Levon Kadehjian <levon.kadehjian.dev@gmail.com>
2025-10-07 14:52:36 -0400

    add Card Name and number details to CheckoutDetails

--- frontend/my-react-app/src/components/CheckoutDetails.tsx
+++ frontend/my-react-app/src/components/CheckoutDetails.tsx
@@ -6,6 +6,28 @@ function CheckoutDetails() {
     return (
         /*Checkout Info*/
         <div className="checkout">
+            <h2>Checkout Information</h2>
+
+            <form>
+                <label>Name on Card</label>
+                <input
+                    required
+                    type="text"
+                    placeholder="John Smith"/>
+
+                <label> Credit card number</label>
+                <input
+                    required
+                    type="text"
+                    inputMode="numeric"
+                    placeholder="1111-2222-3333-4444"
+                    pattern="\d{16}"
+                    maxLength={16}
+                />
+
+
+            </form>
+
         </div>
 
     )
