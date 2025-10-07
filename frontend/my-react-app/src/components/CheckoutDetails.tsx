6fcdff685ae460783bb4a0aee518e778727abc52
Levon Kadehjian <levon.kadehjian.dev@gmail.com>
2025-10-07 14:50:14 -0400

    add CheckoutDetails

---
+++ frontend/my-react-app/src/components/CheckoutDetails.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+function CheckoutDetails() {
+
+
+    return (
+        /*Checkout Info*/
+        <div className="checkout">
+        </div>
+
+    )
+
+}
+
+export default CheckoutDetails
