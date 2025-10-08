025d0ef8e1389c8c9ec924c773df7d0935535bae
Levon Kadehjian <172553116+Levon-Kadehjian@users.noreply.github.com>
2025-10-08 15:50:23 -0400

    Feature/user checkout frontend (#55)
    * add CheckoutDetails
    
    * add Card Name and number details to CheckoutDetails
    
    * add exp date and CVV details to CheckoutDetails
    
    * add action buttons to CheckoutDetails
    
    * add preventing and handling errors
    
    * add CSS file for CheckoutPage
    
    * add TicketDetails file
    
    * add mock event to TicketDetails file
    
    * add CheckoutPage page
    
    * fix comments in CheckoutDetails.tsx
    
    * change css namings in CheckoutPage.css
    
    * update CheckoutDetails classNames and ID
    
    * update CheckoutDetails classNames and ID
    
    * update CheckoutDetails classNames and ID
    
    * update CheckoutPage classNames
    
    * update ticketDetails classNames and ID
    
    * add prevent numeric values in the card name box
    
    * add mock-ticket-image
    
    * add mock-ticket-image
    
    * fix css issues
    
    * remove unused import
    
    * remove extra div
    
    * Update submit button color
    
    * Add React Router with CheckoutPage route setup
    
    * add home page containing(Header - Toolbar - App)
    
    * add react-router-dom
    
    * remove vite unnecessary sections
    
    * add checkout page button to the home page
    
    * update cancel button color
    
    * add separate routes into layouts with and without Header component
    
    * remove unused imports
    
    * add year validation for card expiration date
    
    * fix css

--- frontend/my-react-app/src/main.tsx
+++ frontend/my-react-app/src/main.tsx
@@ -1,19 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import './index.css'
-import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import Header from "./components/Header.tsx";
-import { Toolbar } from "@mui/material";
+
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
           <AuthProvider>
-              <Header />
-              <Toolbar />
               <App />
           </AuthProvider>
       </BrowserRouter>
