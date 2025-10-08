b2e6a0d1d4574ba51f56a11b085808ecf6fd7827
Levon Kadehjian <levon.kadehjian.dev@gmail.com>
2025-10-08 14:00:22 -0400

    remove vite unnecessary sections

--- frontend/my-react-app/src/App.tsx
+++ frontend/my-react-app/src/App.tsx
@@ -1,44 +1,22 @@
 // src/App.tsx
-import { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import SignUp from './SignUp';
 import Login from './Login';
 
 function Home() {
-    const [count, setCount] = useState(0);
     const navigate = useNavigate();
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
                 <button onClick={() => navigate('/signup')}>
                     Go to Sign Up
                 </button>
                 <button onClick={() => navigate('/login')}>
                     Go to Login
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
