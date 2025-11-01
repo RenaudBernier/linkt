5d6c9c11bc8c63e5bc686deb52ebdb49001b466c
Renaud Bernier <renaudbernier@hotmail.fr>
2025-11-01 15:41:41 -0400

    Add ticket scanning functionality with QR code support

--- frontend/my-react-app/src/App.tsx
+++ frontend/my-react-app/src/App.tsx
@@ -24,6 +24,7 @@ import MyTickets from './mytickets.tsx';
 import Settings from "./components/Settings";
 import SavedTickets from "./SavedTickets.tsx";
 import OrganiserApprovePage from "./pages/OrganiserApprovePage.tsx";
+import ScanTicketPage from "./pages/ScanTicketPage.tsx";
 function MainLayout() {
     return (
         <>
@@ -132,6 +133,7 @@ function App() {
                     <Route path="/settings" element={<Settings/>}></Route>
                     <Route path="/savedtickets" element={<SavedTickets/>} />
                     <Route path="/admin/approve-organizer" element={<OrganiserApprovePage />} />
+                    <Route path="/myevents/scan/:eventId" element={<ScanTicketPage />} />
                 </Route>
 
                 <Route element={<BlankLayout/>}>
