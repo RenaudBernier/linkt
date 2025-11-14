ca870dda2be7bf325d1e76716ce92f9e9749e917
MaxIngram05 <160544897+MaxIngram05@users.noreply.github.com>
2025-11-14 11:44:08 -0500

    Add user-specific links to footer and create links module (#143)

---
+++ frontend/my-react-app/src/components/links.ts
@@ -0,0 +1,65 @@
+// src/components/links.ts
+
+export const baseLinks = [
+    { title: 'Product', links: [
+        { label: 'Features', path: '#' },
+        { label: 'Pricing', path: '#' },
+        { label: 'Integrations', path: '#' },
+        { label: 'Documentation', path: '#' }
+    ]},
+    { title: 'Company', links: [
+        { label: 'About Us', path: '#' },
+        { label: 'Careers', path: '#' },
+        { label: 'News', path: '#' },
+        { label: 'Contact', path: '#' }
+    ]},
+    { title: 'Resources',     links: [
+        { label: 'Help Center', path: '#' },
+        { label: 'Community Forum', path: '#' },
+        { label: 'System Status', path: '#' },
+        { label: 'Report an Issue', path: '#' }
+    ]},
+];
+
+export const studentLinks = [
+  { title: 'For Students', links: [
+    { label: 'Browse Events', path: '/events' },
+    { label: 'My Tickets', path: '/mytickets' },
+    { label: 'Saved Tickets', path: '/savedtickets' },
+  ]},
+];
+
+export const organizerLinks = [
+  { title: 'For Organizers', links: [
+    { label: 'Create Events', path: '/events/create' },
+    { label: 'My Events', path: '/my-events' },
+  ]},
+];
+
+export const adminLinks = [
+  { title: 'For Administrators', links: [
+    { label: 'Dashboard', path: '/admin/dashboard' },
+    { label: 'Approve Organizers', path: '/admin/approve-organizer' },
+  ]},
+];
+
+export const defaultLinks = [
+    { title: 'Product',       links: [
+        { label: 'Features', path: '#' },
+        { label: 'Pricing', path: '#' },
+        { label: 'Integrations', path: '#' },
+        { label: 'Documentation', path: '#' }
+    ]},
+    { title: 'Company',       links: [
+        { label: 'About Us', path: '#' },
+        { label: 'Careers', path: '#' },
+        { label: 'News', path: '#' },
+        { label: 'Contact', path: '#' }
+    ]},
+    { title: 'Resources',     links: [
+        { label: 'Help Center', path: '#' },
+        { label: 'Community Forum', path: '#' },
+        { label: 'System Status', path: '#' },
+        { label: 'Report an Issue', path: '#' }
+    ]},
+];
